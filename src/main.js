1e2c1fc06e92b7d29430b6c046d048181f08a877
Makson <144148309+Sky65678@users.noreply.github.com>
2024-07-21 22:44:27 +0300

    rework cath btn

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-import { getImage } from "./js/pixabay-api";
-import { imagesTemplate } from "./js/render-functions";
+import { getImage } from './js/pixabay-api';
+import { imagesTemplate } from './js/render-functions';
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryIt = new SimpleLightbox('.gallery-item a', {
-    captionsData: 'alt',
-    captionDelay: 250,
+  captionsData: 'alt',
+  captionDelay: 250,
 });
 
 const formEL = document.querySelector('.search-form');
@@ -24,86 +24,84 @@ let maxPage = 0;
 let userImg = '';
 
 function showLoader() {
-    loader.classList.remove('hidden');
+  loader.classList.remove('hidden');
 }
 
 function hideLoader() {
-    loader.classList.add('hidden');
+  loader.classList.add('hidden');
 }
 
 function hideBtn() {
-    moreBtnEl.classList.add('hidden');
+  moreBtnEl.classList.add('hidden');
 }
 
 function showBtn() {
-    moreBtnEl.classList.remove('hidden');
+  moreBtnEl.classList.remove('hidden');
 }
 
 formEL.addEventListener('submit', async e => {
-    currentPage = 1;
-    e.preventDefault();
-    userImg = inputEl.value.trim();
-    showLoader();
-    if (userImg === '') {
-        ulElem.innerHTML = '';
-        iziToast.error({
-            title: '',
-            message: 'Enter query to see rezults!',
-            position: 'topRight',            
-        })
-        hideLoader();
-        return;
+  currentPage = 1;
+  e.preventDefault();
+  userImg = inputEl.value.trim();
+  showLoader();
+  if (userImg === '') {
+    ulElem.innerHTML = '';
+    iziToast.error({
+      title: '',
+      message: 'Enter query to see rezults!',
+      position: 'topRight',
+    });
+    hideLoader();
+    return;
+  }
+  try {
+    const { hits, totalHits } = await getImage(userImg, currentPage);
+    maxPage = Math.ceil(totalHits / perPage);
+    console.log(maxPage);
+    if (hits.length === 0) {
+      iziToast.error({
+        title: '',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+        maxWidth: '432px',
+        color: '#ef4040',
+      });
+      ulElem.innerHTML = '';
+      updateBtnStatus();
+      hideLoader();
+      formEL.reset();
+      return;
     }
-    try {
-        const { hits, totalHits } = await getImage(userImg, currentPage);
-        maxPage = Math.ceil(totalHits / perPage);
-        console.log(maxPage);
-        if (hits.length === 0) {
-            iziToast.error({
-                title: '',
-                message:
-                    'Sorry, there are no images matching your search query. Please try again!',
-                position: 'topRight',
-                maxWidth: '432px',
-                color: '#ef4040',
-            })
-            ulElem.innerHTML = '';
-            updateBtnStatus();
-            hideLoader();
-            formEL.reset();
-            return;
-        }
-        const markup = imagesTemplate(hits);
-        ulElem.innerHTML = markup;
-        galleryIt.refresh();
-        hideLoader();
-        formEL.reset();
-    } catch {
-        (err) => {
-            console.log(err);
-        }
+    const markup = imagesTemplate(hits);
+    ulElem.innerHTML = markup;
+    galleryIt.refresh();
+    hideLoader();
+    formEL.reset();
+  } catch {
+    err => {
+      console.log(err);
     };
-    updateBtnStatus();
+  }
+  updateBtnStatus();
 });
 
 moreBtnEl.addEventListener('click', async () => {
-    currentPage++;
-    hideBtn();
-    showLoader();
-    try {
-        const { hits } = await getImage(userImg, currentPage);
-        const markup = imagesTemplate(hits);
-        ulElem.insertAdjacentHTML('beforeend', markup);
-        galleryIt.refresh();
-        scrollGallery();
-        hideLoader();
-    } catch {
-        err => {
-        console.log(err);
-        }
-    }
-    updateBtnStatus();
-})
+  currentPage++;
+  hideBtn();
+  showLoader();
+  try {
+    const { hits } = await getImage(userImg, currentPage);
+    const markup = imagesTemplate(hits);
+    ulElem.insertAdjacentHTML('beforeend', markup);
+    galleryIt.refresh();
+    scrollGallery();
+    hideLoader();
+  } catch (err) {
+    console.log(err);
+  }
+  updateBtnStatus();
+});
 
 function updateBtnStatus() {
   if (currentPage >= maxPage) {
@@ -127,5 +125,3 @@ function scrollGallery() {
     behavior: 'smooth',
   });
 }
-
-
